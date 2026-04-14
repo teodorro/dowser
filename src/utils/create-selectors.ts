c72462fa4560daeba6c8ceb54c76e068fa68e5a8
theo <edem.teo@gmail.com>
2026-04-14 17:48:52 +0300

    fix: as any & lint

--- src/utils/create-selectors.ts
+++ src/utils/create-selectors.ts
@@ -5,11 +5,12 @@ type WithSelectors<S> = S extends { getState: () => infer T }
   : never;
 
 export const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
-  _store: S
+  _store: S,
 ) => {
   const store = _store as WithSelectors<typeof _store>;
   store.use = {};
   for (const k of Object.keys(store.getState())) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (store.use as any)[k] = () => store((s) => s[k as keyof typeof s]);
   }
 
